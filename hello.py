c49ade95d15998bd35a617069503d41df91d6866
arthurliu <theonlyarthurliu@gmail.com>
2020-10-17 15:21:33 -0700

    skeleton

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, url_for, request, redirect
-import sqlite3
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-   return render_template('index.html')
+def hello_world():
+  return 'Hello, World!'
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+  app.run(host="0.0.0.0", port=8080, debug=False)
