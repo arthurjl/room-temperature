2f8c28204141a67bc24a7df28fcafa3d434fb804
arthurliu <theonlyarthurliu@gmail.com>
2020-10-17 15:14:45 -0700

    push for andrew

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+import sqlite3
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
-
+def home():
+   return render_template('index.html')
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=True)
