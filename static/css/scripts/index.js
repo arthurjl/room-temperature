0fa6be68be1de12eb05fdf660cc164bc3b45959b
Jake Jung <junyong217@hotmail.com>
2020-10-18 00:05:16 -0700

    Added index.js
    Added Swup compatibility

---
+++ static/css/scripts/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import Swup from 'swup';
+const swup = new Swup();
+
+(function() {
+	window.addEventListener("load", init);
+
+	function init() {
+		print("Hello World");
+	}
+})();
