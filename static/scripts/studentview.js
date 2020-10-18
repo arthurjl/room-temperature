c50323581c852495d6beafbf04c8ca7b59e9d860
Daniel Zhu <dzhu99@cs.washington.edu>
2020-10-18 02:45:09 -0700

    bigger face

--- static/scripts/studentview.js
+++ static/scripts/studentview.js
@@ -11,7 +11,7 @@
     const interval = setInterval(function() {
       udpateTherm1("#happy_button")
       updateBigFace()
-    }, 1000);
+    }, 500);
 
     qs("#happy_button").addEventListener("click", () => postToEmotions("#happy_button"));
     qs("#sad_button").addEventListener("click", () => postToEmotions("#sad_button"));
