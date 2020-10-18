ed136e810e45a9783f6615f24ba95f3b122ba6f2
Jake Jung <junyong217@hotmail.com>
2020-10-18 03:02:58 -0700

    Update index.js
    QS go brrrrr

--- static/css/scripts/index.js
+++ static/css/scripts/index.js
@@ -16,4 +16,13 @@ const swup = new Swup();
 		localStorage.setItem('data', JSON.stringify(str));
 		console.log("Saved class name to local storage")
 	}
+
+	/**
+   * Returns first element matching selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} - DOM object associated selector.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
 })();
